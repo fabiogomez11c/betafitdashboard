8431c5a29d73fff8d88f5387b50bf0db5981348f
Fabio Gomez <fabiogomez11c@gmail.com>
2021-09-13 09:44:27 -0500

    First mock of the sidebar

--- src/components/dashboard/DashboardScreen.js
+++ src/components/dashboard/DashboardScreen.js
@@ -11,11 +11,39 @@ export const DashboardScreen = () => {
     }
     
     return (
-        <div>
-            <h1>DashboardScreen</h1>
-            <button onClick={handleLogoutClick}>
-                Logout
-            </button>
-        </div>
+        <section className='dashboard'>
+            <div className='dashboard__sidebar sidebar'>
+                {/* 
+                    A whole component (probably with subcomponents)
+                    that represents the side bar, from the button to open
+                    the side bar, to each of the options to navigate 
+                    in the dashboard.
+
+                    - button
+                    - profile
+                    -- picture
+                    -- name
+                    - option A
+                    - option B
+                */}
+                <button className='sidebar__button'>
+                    CO
+                </button>
+                <div className='sidebar__profile'>
+                    <img alt='profile__picture'/>
+                    <p>Fabio Gomez</p>
+                </div>
+                <div >
+                    <p>Option A</p>
+                    <p>Option B</p>
+                </div>
+            </div>
+            <div className='dashboard__view'>
+                <h1>DashboardScreen</h1>
+                <button onClick={handleLogoutClick}>
+                    Logout
+                </button>
+            </div>
+        </section>
     )
 }
